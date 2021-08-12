6c376acbad6f10ef1821be98e90864428a747561
Carolyn Lau <thecarolynlau@gmail.com>
2021-08-12 15:14:10 -0700

    deleted React boilerplate files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Form from './components/Form';
+import List from './components/List';
 
-function App() {
+const App = () => {
+  // states
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [edit, setEdit] = useState(false);
+
+  // effects
+
+  // run once when app starts
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  // functions
+  const filterHandler = () => {
+    switch (status) {
+      case 'complete':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case 'incomplete':
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  // save to local data
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  // get local data
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todosLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todosLocal);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <div className="title">
+          <h1 className="title__name">My To-Do List</h1>
+        </div>
+
+        <Form
+          inputText={inputText}
+          todos={todos}
+          setInputText={setInputText}
+          setTodos={setTodos}
+          setStatus={setStatus}
+        />
+
+        <List
+          edit={edit}
+          filteredTodos={filteredTodos}
+          todos={todos}
+          setEdit={setEdit}
+          setTodos={setTodos}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
